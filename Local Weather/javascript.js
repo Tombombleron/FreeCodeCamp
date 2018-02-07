760be8664e6df58895e6e0986650908a822ee638
Tombombleron <tombombleron@gmail.com>
2018-02-07 19:52:36 +0000

    updated js and css

--- Local Weather/javascript.js
+++ Local Weather/javascript.js
@@ -1,11 +1,14 @@
+var temp = "C";
+var lat;
+var lon;
 $(document).ready(function() {
     getLocation();
 });
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      var lat = "lat=" + position.coords.latitude;
-      var lon = "lon=" + position.coords.longitude;
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
        getWeather(lat, lon);
     });
   } else {
@@ -13,9 +16,40 @@ function getLocation() {
   }
 }
 function getWeather(lat, lon) {
-    $.getJSON("https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon, function(json) {
-        $("#weather-str").html(json.weather[0].description);
-        $("#weather-location").html(json.name + ", " + json.sys.country);
+    $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon, function(json) {
+        $("#weather-str").html();
+        $("#location-str").html(json.name + ", " + json.sys.country + " | " + json.weather[0].description);
         $("#img-file").attr("src",json.weather[0].icon);
+        $("#js_temp").html(Math.round(json.main.temp));
     });
 }
+$("#degrees").click(function() {
+    getTemp(lat, lon);
+    
+});
+function getTemp(lat, lon) {
+    $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon, function(json) {
+        if (temp=="C") {
+            $("body").animate({"background-color": "#2BBBAD"}, 3000, function(){});
+            $("#temp").fadeOut(1500);
+            $("#temp").fadeIn(1500);
+            setTimeout(function(){
+                $("#js_temp").html((1.8*Math.round(json.main.temp))+32);
+                $("#degrees").html("°F");
+                temp = "F";
+            }, 1500);
+            
+//window.alert(
+        } else {
+            $("#temp").fadeOut(1500);
+            $("#temp").fadeIn(1500);
+            $("body").animate({"background-color": "#00695c"}, 3000, function(){});
+            setTimeout(function(){
+                $("#js_temp").html((Math.round(json.main.temp)));
+                $("#degrees").html("°C");
+                temp = "C";
+            }, 1500);
+        }
+//        Math.round(json.main.temp);
+    });
+  }
